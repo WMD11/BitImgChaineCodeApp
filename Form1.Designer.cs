59bcce8e88e60341874766cc48b8484979686cce
WMD11 <113972186+WMD11@users.noreply.github.com>
2024-02-16 01:46:50 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace BitImgChaineCodeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadImage1Button = new System.Windows.Forms.Button();
+            this.LoadImage2Button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.GenerateChainCode1Button = new System.Windows.Forms.Button();
+            this.GenerateChainCode2Button = new System.Windows.Forms.Button();
+            this.chainCode1TextBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.ApplyLogicalOperationButton = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SaveResultButton = new System.Windows.Forms.Button();
+            this.ConvertToBinaryImageButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadImage1Button
+            // 
+            this.LoadImage1Button.Location = new System.Drawing.Point(12, 13);
+            this.LoadImage1Button.Name = "LoadImage1Button";
+            this.LoadImage1Button.Size = new System.Drawing.Size(96, 23);
+            this.LoadImage1Button.TabIndex = 0;
+            this.LoadImage1Button.Text = "Load Image 1";
+            this.LoadImage1Button.UseVisualStyleBackColor = true;
+            // 
+            // LoadImage2Button
+            // 
+            this.LoadImage2Button.Location = new System.Drawing.Point(12, 42);
+            this.LoadImage2Button.Name = "LoadImage2Button";
+            this.LoadImage2Button.Size = new System.Drawing.Size(97, 23);
+            this.LoadImage2Button.TabIndex = 1;
+            this.LoadImage2Button.Text = "Load Image 2";
+            this.LoadImage2Button.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(306, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(442, 429);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(793, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(442, 429);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(306, 462);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(442, 429);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // GenerateChainCode1Button
+            // 
+            this.GenerateChainCode1Button.Location = new System.Drawing.Point(12, 90);
+            this.GenerateChainCode1Button.Name = "GenerateChainCode1Button";
+            this.GenerateChainCode1Button.Size = new System.Drawing.Size(154, 23);
+            this.GenerateChainCode1Button.TabIndex = 5;
+            this.GenerateChainCode1Button.Text = "Generate Chain Code 1";
+            this.GenerateChainCode1Button.UseVisualStyleBackColor = true;
+            // 
+            // GenerateChainCode2Button
+            // 
+            this.GenerateChainCode2Button.Location = new System.Drawing.Point(12, 119);
+            this.GenerateChainCode2Button.Name = "GenerateChainCode2Button";
+            this.GenerateChainCode2Button.Size = new System.Drawing.Size(154, 23);
+            this.GenerateChainCode2Button.TabIndex = 6;
+            this.GenerateChainCode2Button.Text = "Generate Chain Code 2";
+            this.GenerateChainCode2Button.UseVisualStyleBackColor = true;
+            // 
+            // chainCode1TextBox
+            // 
+            this.chainCode1TextBox.Location = new System.Drawing.Point(12, 170);
+            this.chainCode1TextBox.Name = "chainCode1TextBox";
+            this.chainCode1TextBox.Size = new System.Drawing.Size(154, 20);
+            this.chainCode1TextBox.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 196);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "AND",
+            "NOT ",
+            "AndNot",
+            "OR",
+            "XOR"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 240);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(122, 79);
+            this.checkedListBox1.TabIndex = 10;
+            // 
+            // ApplyLogicalOperationButton
+            // 
+            this.ApplyLogicalOperationButton.Location = new System.Drawing.Point(12, 344);
+            this.ApplyLogicalOperationButton.Name = "ApplyLogicalOperationButton";
+            this.ApplyLogicalOperationButton.Size = new System.Drawing.Size(154, 23);
+            this.ApplyLogicalOperationButton.TabIndex = 11;
+            this.ApplyLogicalOperationButton.Text = "Apply Logical Operation";
+            this.ApplyLogicalOperationButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 399);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(153, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // SaveResultButton
+            // 
+            this.SaveResultButton.Location = new System.Drawing.Point(12, 492);
+            this.SaveResultButton.Name = "SaveResultButton";
+            this.SaveResultButton.Size = new System.Drawing.Size(96, 23);
+            this.SaveResultButton.TabIndex = 13;
+            this.SaveResultButton.Text = "Save Result";
+            this.SaveResultButton.UseVisualStyleBackColor = true;
+            // 
+            // ConvertToBinaryImageButton
+            // 
+            this.ConvertToBinaryImageButton.Location = new System.Drawing.Point(11, 452);
+            this.ConvertToBinaryImageButton.Name = "ConvertToBinaryImageButton";
+            this.ConvertToBinaryImageButton.Size = new System.Drawing.Size(154, 23);
+            this.ConvertToBinaryImageButton.TabIndex = 14;
+            this.ConvertToBinaryImageButton.Text = "Generate Chain Code 1";
+            this.ConvertToBinaryImageButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1247, 903);
+            this.Controls.Add(this.ConvertToBinaryImageButton);
+            this.Controls.Add(this.SaveResultButton);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.ApplyLogicalOperationButton);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.chainCode1TextBox);
+            this.Controls.Add(this.GenerateChainCode2Button);
+            this.Controls.Add(this.GenerateChainCode1Button);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LoadImage2Button);
+            this.Controls.Add(this.LoadImage1Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadImage1Button;
+        private System.Windows.Forms.Button LoadImage2Button;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button GenerateChainCode1Button;
+        private System.Windows.Forms.Button GenerateChainCode2Button;
+        private System.Windows.Forms.TextBox chainCode1TextBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button ApplyLogicalOperationButton;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button SaveResultButton;
+        private System.Windows.Forms.Button ConvertToBinaryImageButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitImgChaineCodeApp
+{
+    public partial class Form1 : Form
+
+    {
+
+        private int[] directionsX = { 1, 1, 0, -1, -1, -1, 0, 1 };
+        private int[] directionsY = { 0, -1, -1, -1, 0, 1, 1, 1 };
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeUI();
+
+        }
+
+        private void InitializeUI()
+        {
+            // Inițializează elementele UI aici
+            // Adaugă evenimentele pentru butoane
+            LoadImage1Button.Click += LoadImage1Button_Click;
+            GenerateChainCode1Button.Click += GenerateChainCode1Button_Click;
+            LoadImage2Button.Click += LoadImage2Button_Click;
+            GenerateChainCode2Button.Click += GenerateChainCode2Button_Click;
+            ApplyLogicalOperationButton.Click += ApplyLogicalOperationButton_Click;
+            ConvertToBinaryImageButton.Click += ConvertToBinaryImageButton_Click;
+            SaveResultButton.Click += SaveResultButton_Click;
+
+        }
+
+        private void LoadImage1Button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Imagini (*.bmp, *.png, *.jpg, *.jpeg)|*.bmp;*.png;*.jpg;*.jpeg|Toate fișierele (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Încarcă imaginea folosind Bitmap
+                    Bitmap image1 = new Bitmap(openFileDialog.FileName);
+
+                    // Verifică dacă imaginea este alb-negru (1 bit) sau tonuri de gri (8 biți)
+                    if (ImageIsBinary(image1))
+                    {
+                        // Afișează imaginea în PictureBox1
+                        pictureBox1.Image = image1;
+                        pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+                        // Adaugă codul suplimentar pentru a gestiona chain code-ul imaginii 1, dacă este necesar
+                        // Chain code-ul poate fi generat și afișat în TextBox-ul corespunzător
+                    }
+                    else
+                    {
+                        MessageBox.Show("Imaginea trebuie să fie alb-negru (1 bit) sau tonuri de gri (8 biți).", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Eroare la încărcarea imaginii: " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private bool ImageIsBinary(Bitmap image)
+        {
+            if (image.PixelFormat == System.Drawing.Imaging.PixelFormat.Format1bppIndexed ||
+                image.PixelFormat == System.Drawing.Imaging.PixelFormat.Format8bppIndexed)
+            {
+                return true;
+            }
+            else if (image.PixelFormat == System.Drawing.Imaging.PixelFormat.Format24bppRgb ||
+                     image.PixelFormat == System.Drawing.Imaging.PixelFormat.Format32bppArgb)
+            {
+                // Verifică dacă este o imagine gri (toate canalele de culoare sunt egale)
+                for (int x = 0; x < image.Width; x++)
+                {
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        System.Drawing.Color pixelColor = image.GetPixel(x, y);
+                        if (pixelColor.R != pixelColor.G || pixelColor.G != pixelColor.B)
+                        {
+                            return false; // Imaginea nu este gri
+                        }
+                    }
+                }
+                return true; // Imaginea este gri
+            }
+
+            return false; // Alte formate de pixel
+        }
+
+        private void GenerateChainCode1Button_Click(object sender, EventArgs e)
+        {
+            // Verifică dacă există o imagine în PictureBox1
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Încărcați o imagine înainte de a genera chain code-ul.", "Atenție", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Obține imaginea din PictureBox1
+            Bitmap image1 = new Bitmap(pictureBox1.Image);
+
+            // Verifică dacă imaginea este alb-negru (1 bit) sau tonuri de gri (8 biți)
+            if (!ImageIsBinary(image1))
+            {
+                MessageBox.Show("Imaginea trebuie să fie alb-negru (1 bit) sau tonuri de gri (8 biți).", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Generare chain code pentru imaginea 1
+            string chainCode1 = GenerateChainCode(image1);
+
+            // Afișează chain code-ul în TextBox-ul corespunzător
+            chainCode1TextBox.Text = chainCode1;
+        }
+        private string GenerateChainCode(Bitmap image)
+        {
+            Bitmap binaryImage = ConvertToBinary(image);
+            Point startPoint = FindStartPoint(binaryImage);
+
+            if (startPoint == Point.Empty)
+            {
+                MessageBox.Show("Obiectul nu a fost găsit în imagine.", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return string.Empty;
+            }
+
+            StringBuilder chainCode = new StringBuilder();
+            int currentX = startPoint.X;
+            int currentY = startPoint.Y;
+            int currentDirection = 0;
+
+            do
+            {
+                int nextDirection = FindNextDirection(binaryImage, currentX, currentY, currentDirection);
+                chainCode.Append(nextDirection);
+                currentDirection = nextDirection;
+
+                int[] delta = { directionsX[currentDirection], directionsY[currentDirection] };
+                currentX += delta[0];
+                currentY += delta[1];
+
+            } while (currentX != startPoint.X || currentY != startPoint.Y);
+
+            return chainCode.ToString();
+        }
+
+        private Bitmap ConvertToBinary(Bitmap image)
+        {
+            // Convertim imaginea la alb-negru
+            Bitmap binaryImage = new Bitmap(image.Width, image.Height);
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    System.Drawing.Color pixelColor = image.GetPixel(x, y);
+                    int grayscaleValue = (int)(pixelColor.R * 0.3 + pixelColor.G * 0.59 + pixelColor.B * 0.11);
+
+                    if (grayscaleValue < 128)
+                    {
+                        binaryImage.SetPixel(x, y, System.Drawing.Color.Black);
+                    }
+                    else
+                    {
+                        binaryImage.SetPixel(x, y, System.Drawing.Color.White);
+                    }
+                }
+            }
+
+            return binaryImage;
+        }
+
+        private Point FindStartPoint(Bitmap image)
+        {
+            // Găsim primul pixel negru în imagine (punctul de start al conturului)
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    if (image.GetPixel(x, y) == System.Drawing.Color.Black)
+                    {
+                        return new Point(x, y);
+                    }
+                }
+            }
+
+            return Point.Empty; // Nu am găsit un punct de start valid
+        }
+
+        private int FindNextDirection(Bitmap image, int x, int y, int currentDirection)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                int nextDirection = (currentDirection + i) % 8;
+                int nextX = x + directionsX[nextDirection];
+                int nextY = y + directionsY[nextDirection];
+
+                if (nextX >= 0 && nextX < image.Width && nextY >= 0 && nextY < image.Height && image.GetPixel(nextX, nextY) == System.Drawing.Color.Black)
+                {
+                    return nextDirection;
+                }
+            }
+
+            return currentDirection;
+        }
+
+
+        private void LoadImage2Button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Imagini (*.bmp, *.png, *.jpg, *.jpeg)|*.bmp;*.png;*.jpg;*.jpeg|Toate fișierele (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Încarcă imaginea folosind Bitmap
+                    Bitmap image2 = new Bitmap(openFileDialog.FileName);
+
+                    // Verifică dacă imaginea este alb-negru (1 bit) sau tonuri de gri (8 biți)
+                    if (ImageIsBinary(image2))
+                    {
+                        // Afișează imaginea în PictureBox2
+                        pictureBox2.Image = image2;
+                        pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+                        // Adaugă codul suplimentar pentru a gestiona chain code-ul imaginii 2, dacă este necesar
+                        // Chain code-ul poate fi generat și afișat în TextBox-ul corespunzător
+                    }
+                    else
+                    {
+                        MessageBox.Show("Imaginea trebuie să fie alb-negru (1 bit) sau tonuri de gri (8 biți).", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Eroare la încărcarea imaginii: " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        private void GenerateChainCode2Button_Click(object sender, EventArgs e)
+        {
+            // Implementează codul pentru a genera chain code-ul pentru a doua imagine
+            // Afișează rezultatul în TextBox-ul corespunzător
+        }
+
+        private void ApplyLogicalOperationButton_Click(object sender, EventArgs e)
+        {
+            // Implementează codul pentru a gestiona operațiile logice selectate
+            // Afișează rezultatul în TextBox-ul corespunzător
+        }
+
+        private void ConvertToBinaryImageButton_Click(object sender, EventArgs e)
+        {
+            // Implementează codul pentru a converti chain code-ul rezultat în imagine binară
+            // Afișează rezultatul în PictureBox-ul corespunzător
+        }
+
+        private void SaveResultButton_Click(object sender, EventArgs e)
+        {
+            // Implementează codul pentru a salva chain code-ul rezultat într-un fișier text
+            // Poți utiliza SaveFileDialog pentru a permite utilizatorului să selecteze locația și numele fișierului.
+        }
+    }
+}
